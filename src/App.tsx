98ccca8d2d8575b0ecfe95259fb83b5fbd0d56d6
LeoYao <leoyao98@gmail.com>
2021-05-30 18:01:26 +1200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import { Route, Switch } from 'react-router'
+import Login from './page/login/login'
+import Admin from './page/admin/admin'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/admin' component={Admin} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/page/admin/admin.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export interface AdminProps {}
+
+export interface AdminState {}
+
+class Admin extends Component<AdminProps, AdminState> {
+  render() {
+    return <div>xxxx</div>
+  }
+}
+
+export default Admin
