de7b9760b8373296af816309b49469f7753739bd
LeoYao <leoyao98@gmail.com>
2021-05-31 22:56:35 +1200

    redux init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React,{Component} from 'react';
-import { Route, Switch } from 'react-router'
-import Login from './page/login/login'
-import Admin from './page/admin/admin'
-
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route path='/login' component={Login} />
-          <Route path='/admin' component={Admin} />
-        </Switch>
+        App
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'antd/dist/antd.less'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <App />,
   document.getElementById('root')
 );
 
